c979b08d956efd57adbb0222100b7c5a8c601b4e
kaichaosun <kaichaosuna@gmail.com>
2025-03-18 12:34:26 +0800

    init code

---
+++ entrypoints/popup/App.tsx
@@ -0,0 +1,35 @@
+import reactLogo from '@/assets/react.svg';
+import { useState } from 'react';
+import './App.css';
+import WakuConnectLogo from '/waku-connect-logo.svg';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://wxt.dev" target="_blank">
+          <img src={WakuConnectLogo} className="logo" alt="WXT logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>WXT + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the WXT and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
